62c6d8ee3e585a549324a30aef872d6adf19b41d
SOSA <SOSA@DESKTOP-V4PQRV0>
2024-05-24 20:56:07 -0300

    Completando las clases, con getters and setters

--- tp4_SosaFacundoGabriel/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_SosaFacundoGabriel/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -8,6 +8,44 @@ public class Docente {
 	private String apellido;
 	private String email;
 	private int telefono;
+	public Docente(String legajo, String nombre, String apellido, String email, int telefono) {
+		super();
+		this.legajo = legajo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+	}
+	public String getLegajo() {
+		return legajo;
+	}
+	public void setLegajo(String legajo) {
+		this.legajo = legajo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
 	
 	
 }
